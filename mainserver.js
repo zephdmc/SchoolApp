e2c6fc5d3a9c00f6b3191ab1089e2c9cf59302a1
zephdmc <zephdmc@gmail.com>
2025-09-03 14:10:25 +0100

    Your commit message here

--- mainserver.js
+++ mainserver.js
@@ -30,7 +30,7 @@ app.use('/academic', academic);
 app.use('/fee', payment);
 
 // Serve index.html for React frontend routes
-app.get('/payment/:id', (req, res) => {
+app.get('/portal/payment/:id', (req, res) => {
     res.sendFile(path.join(__dirname, './portal-itc/build', 'index.html'), (err) => {
         if (err) {
             console.error('Error serving frontend:', err);
