caeb57e4d5709b2521ca6b7d583b5c6aab2bf458
zephdmc <zephdmc@gmail.com>
2025-09-22 12:14:31 +0100

    Update StudentPage.js
    vbvb

--- portal-itc/src/Pages/StudentPage.js
+++ portal-itc/src/Pages/StudentPage.js
@@ -20,7 +20,7 @@ import ClassSubjectsView from './StudentPage/MySubjects';
 
 const sections = {
   'Overview': { component: CalendarEvent, icon: FaTh },
-      'My Subjects': { component: ClassSubjectsView, icon: FaUsers },
+  'My Subjects': { component: ClassSubjectsView, icon: FaUsers },
   'My Profile': { component: MyProfile, icon: FaUser },
   'Result Checker': { component: ResultChecker, icon: FaFileAlt },
   'Bursery': { component: StudentPayments, icon: FaUsers },
